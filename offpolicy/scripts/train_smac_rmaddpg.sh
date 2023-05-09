5914b66ff867cc233a5a16a66448dc898631bddf
strrules105 <camobot3@gmail.com>
2023-05-09 00:41:01 -0400

    can run rmaddpg

--- offpolicy/scripts/train_smac_rmaddpg.sh
+++ offpolicy/scripts/train_smac_rmaddpg.sh
@@ -9,6 +9,9 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is 
 
 for seed in $(seq ${seed_max}); do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=2 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} --lr 5e-4 --buffer_size 5000 --batch_size 32 --actor_train_interval_step 1 --tau 0.005 --num_env_steps 10000000 --log_interval 20000 --user_name "zoeyuchao"
+    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} \
+     --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} \
+     --seed ${seed} --lr 5e-4 --buffer_size 5000 --batch_size 32 --actor_train_interval_step 1 \
+     --tau 0.005 --num_env_steps 10000000 --log_interval 20000
     echo "training is done!"
 done
